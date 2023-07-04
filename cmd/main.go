923e7ea6ed183ecce76b8934f96545724000e91e
木瓜丸 <boke0@boke0ya.com>
2023-07-04 18:47:19 +0900

    update

--- cmd/main.go
+++ cmd/main.go
@@ -9,8 +9,40 @@ import (
 )
 
 func main() {
+    r := 100
+    n := 5
+
     entities := []AttAlice{}
-    for i := 0; i<5; i++ {
+    for i := 0; i<n; i++ {
         entities = append(entities, NewAttAlice())
+    }
+
+    var message AttMessage
+
+    for i := 0; i<r; i++ {
+        /** # 送信 **/
+        /** ## 準備 **/
+        // i番目のEntity視点で、AliceとBobに分ける
+        alice := entities[i]
+        bobs := map[string]AttBob{}
+        for _, entity := range entities {
+            if entity.Id != alice.Id {
+                bobs[entity.Id] = entity.ToBob()
+            }
+        }
+        // i番目のEntity視点で、Treeを作る
+
+        /** # 受信 **/
+        for j := 0; j<n; i++ {
+            /** ## 準備 **/
+            // i番目のEntity視点で、AliceとBobに分ける
+            alice := entities[i]
+            bobs := map[string]AttBob{}
+            for _, entity := range entities {
+                if entity.Id != alice.Id {
+                    bobs[entity.Id] = entity.ToBob()
+                }
+            }
+        }
     }
 }
